4e0c3a6d568795890aadd5df8dfc661c748c9f66
Kasymkhan <kasymkhankhubievnis@gmail.com>
2025-07-12 14:58:29 +0300

    implemented

---
+++ app/main.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from .core import config
+from .telebot import bot, check_events, dp, on_startup, scheduler
+
+
+async def main():
+
+    # Start scheduler for notifications
+    scheduler.add_job(check_events, "interval", minutes=1)
+    scheduler.start()
+
+    if config.INTERFACE == "telegram":
+        asyncio.create_task(
+            dp.start_polling(bot, on_startup=on_startup),
+        )
+
+    elif config.INTERFACE == "gradio":
+        # gradio_app.launch(server_name="0.0.0.0", server_port=7860)
+        pass
+    else:
+        raise ValueError("Нужно указать интерфейс!")
+
+
+if __name__ == "__main__":
+    # asyncio.run(main())
+    asyncio.run(dp.start_polling(bot, on_startup=on_startup))
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from it-round!")
-
-
-if __name__ == "__main__":
-    main()
